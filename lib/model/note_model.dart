a9f79ea386f160aef631929595cd57f5316b1526
hungrybear6856 <hungrybear6856@gmail.com>
2023-05-31 07:47:57 +0300

    create note Model & generat this note model

---
+++ lib/model/note_model.dart
@@ -0,0 +1,17 @@
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subtitle;
+  @HiveField(2)
+  final String Date;
+  @HiveField(3)
+  final int color;
+
+  NoteModel(this.title, this.subtitle, this.Date, this.color);
+}
