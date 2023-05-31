30b38e921959f36ad48d17ea17f6f7929285aad9
hungrybear6856 <hungrybear6856@gmail.com>
2023-05-31 07:53:45 +0300

    int hive & open note box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:mynote/model/note_model.dart';
 import 'package:mynote/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("note_box");
+
   runApp(const MyApp());
 }
 
